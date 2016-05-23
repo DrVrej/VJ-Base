369d0802147d85fb89ede3950f5e7a9495c5b67b
DrVrej <vrejjamsakian@gmail.com>
2016-05-23 00:21:46 -0400

    Add files via upload

---
+++ weapons/gmod_tool/stools/tool_vj_teleport.lua
@@ -0,0 +1,11 @@
+TOOL.Name = "Teleport Creator"
+TOOL.Category = "Tools"
+TOOL.Tab = "DrVrej"
+TOOL.Command = nil
+TOOL.ConfigName = ""
+
+if (CLIENT) then
+	language.Add("tool.tool_vj_teleport.name", "Teleport Creator")
+	language.Add("tool.tool_vj_teleport.desc", "Create a teleport gate")
+	language.Add("tool.tool_vj_teleport.0", "Left-Click to create point 1")
+end
