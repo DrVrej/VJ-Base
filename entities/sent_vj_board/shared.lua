06645fd576943ba1046314156fba72d475d21169
DrVrej <DrVrej@users.noreply.github.com>
2016-05-23 00:33:16 -0400

    Test

--- entities/sent_vj_board/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type 			= "anim"
-ENT.Base 			= "base_anim"
-ENT.PrintName		= "Wood Board"
-ENT.Author			= "DrVrej"
-ENT.Information		= ""
-ENT.Category		= "VJ Base"
-
-ENT.Spawnable		= false
-ENT.AdminSpawnable	= false
