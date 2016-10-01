ab142c253d40871a317da3c69a5a1e3664a3f4b1
DrVrej <DrVrej@users.noreply.github.com>
2016-10-01 01:59:58 -0400

    Fixes for SNPC dissolving + Added VJ_RoundToMultiple
    Fixes for SNPC dissolving + Added VJ_RoundToMultiple

--- lua/entities/npc_vj_human_base/init.lua
+++ lua/entities/npc_vj_human_base/init.lua
@@ -161,7 +161,6 @@ ENT.DeathNoticePosition = HUD_PRINTCENTER -- Were you want the message to show. 
 ENT.DeathNoticeWriting = "Example: Spider Queen Has Been Defeated!" -- Message that will appear
 ENT.UsesBoneAngle = true -- This can be used to stop the corpse glitching or flying on death
 ENT.UsesDamageForceOnDeath = true -- Disables the damage force on death | Useful for SNPCs with Death Animations
-ENT.IgnoreCBDeath = false -- Ignores the combine ball death | useful for SNPCs that have gibs on death, example: Boomer
 ENT.DropWeaponOnDeath = true -- Should it drop its weapon on death?
 ENT.DropWeaponOnDeathAttachment = "anim_attachment_RH" -- Which attachment should it use for the weapon's position
 ENT.HasItemDropsOnDeath = true -- Should it drop items on death?
@@ -2956,7 +2955,7 @@ function ENT:OnTakeDamage(dmginfo,data,hitgroup)
 
 	if self:Health() <= 0 && self.Dead == false then
 		self:RemoveEFlags(EFL_NO_DISSOLVE)
-		if dmginfo:GetDamageType() == DMG_DISSOLVE then
+		if (dmginfo:GetDamageType() == DMG_DISSOLVE) or (IsValid(dmginfo:GetInflictor()) && dmginfo:GetInflictor():GetClass() == "prop_combine_ball") then
 			local dissolve = DamageInfo()
 			dissolve:SetDamage(self:Health())
 			dissolve:SetAttacker(dmginfo:GetAttacker())
@@ -3106,8 +3105,8 @@ function ENT:PriorToKilled(dmginfo,hitgroup)
 	self:DeathSoundCode()
 	if self.HasDeathAnimation != true then DoKilled() return end
 	if self.HasDeathAnimation == true then
-		if GetConVarNumber("vj_npc_nodeathanimation") == 1 or GetConVarNumber("ai_disabled") == 1 or dmginfo:GetDamageType() == DMG_DISSOLVE then DoKilled() return end
-			if dmginfo:GetDamageType() != DMG_DISSOLVE then
+		if GetConVarNumber("vj_npc_nodeathanimation") == 1 or GetConVarNumber("ai_disabled") == 1 or ((dmginfo:GetDamageType() == DMG_DISSOLVE) or (IsValid(dmginfo:GetInflictor()) && dmginfo:GetInflictor():GetClass() == "prop_combine_ball")) then DoKilled() return end
+			if (dmginfo:GetDamageType() != DMG_DISSOLVE) && (IsValid(dmginfo:GetInflictor()) && dmginfo:GetInflictor():GetClass() != "prop_combine_ball") then
 			local randanim = math.random(1,self.DeathAnimationChance)
 			if randanim != 1 then DoKilled() return end
 			if randanim == 1 then
