70b1a085ac9b1ff251a08b3fb46c41004b461246
DrVrej <DrVrej@users.noreply.github.com>
2018-10-14 14:09:50 -0400

    Gib System Updates
    Gib System Updates

--- lua/entities/npc_vj_creature_base/init.lua
+++ lua/entities/npc_vj_creature_base/init.lua
@@ -4013,13 +4013,17 @@ function ENT:CreateGibEntity(Ent,Models,Tbl_Features,CustomCode)
 	end
 	vTbl_AngleVelocity = vTbl_Features.AngVel or Vector(math.Rand(-200,200),math.Rand(-200,200),math.Rand(-200,200)) -- Angle velocity, how fast it rotates as it's flying
 	vTbl_BloodType = vTbl_Features.BloodType or vTbl_BloodType -- Certain entities such as the VJ Gib entity, you can use this to set its gib type
+	vTbl_BloodDecal = vTbl_Features.BloodDecal or "Default" -- The decal it spawns when it collides with something, leave empty to let the base decide
 	vTbl_NoFade = vTbl_Features.NoFade or false -- Should it fade away and delete?
 	vTbl_RemoveOnCorpseDelete = vTbl_Features.RemoveOnCorpseDelete or false -- Should the entity get removed if the corpse is removed?
 	local gib = ents.Create(Ent)
 	gib:SetModel(Models)
 	gib:SetPos(vTbl_Position)
 	gib:SetAngles(vTbl_Angle)
-	if gib:GetClass() == "obj_vj_gib" then gib.BloodType = vTbl_BloodType end
+	if gib:GetClass() == "obj_vj_gib" then
+		gib.BloodType = vTbl_BloodType
+		gib.Collide_Decal = vTbl_BloodDecal
+	end
 	gib:Spawn()
 	gib:Activate()
 	gib.IsVJBase_Gib = true
