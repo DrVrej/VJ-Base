1ca587cc010f198055958bcb5b09bdf77729777f
DrVrej <DrVrej@users.noreply.github.com>
2019-02-08 16:11:57 -0500

    Version 2.7.1 - Tank Updates + Optimizations
    Version 2.7.1 - Tank Updates + Optimizations

--- lua/entities/npc_vj_tankg_base/init.lua
+++ lua/entities/npc_vj_tankg_base/init.lua
@@ -5,54 +5,52 @@ include('shared.lua')
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
-
-	-- Default ---------------------------------------------------------------------------------------------------------------------------------------------
-ENT.Model = {} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ Core Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ENT.StartHealth = 0
-//ENT.MoveType = MOVETYPE_NONE
 ENT.HullType = HULL_TINY
 ENT.HullSizeNormal = false -- set to false to cancel out the self:SetHullSizeNormal()
 ENT.HasSetSolid = false -- set to false to disable SetSolid
 ENT.VJ_IsHugeMonster = true -- Is this a huge monster?
 ENT.MovementType = VJ_MOVETYPE_STATIONARY -- How does the SNPC move?
 ENT.CanTurnWhileStationary = false -- If set to true, the SNPC will be able to turn while it's a stationary SNPC
----------------------------------------------------------------------------------------------------------------------------------------------
-ENT.HasAllies = true -- Put to false if you want it not to have any allies
-ENT.Bleeds = false -- Does the SNPC bleed? (Blood decal, particle, etc.)
-ENT.HasMeleeAttack = false -- Should the SNPC have a melee attack?
-ENT.DisableWeapons = true -- If set to true, it won't be able to use weapons
-ENT.DisableInitializeCapabilities = true -- If true, it will disable the initialize capabilities, this will allow you to add your own
-ENT.DisableSelectSchedule = true -- Disables Schedule code, Custom Schedule can still work
-ENT.HasPainSounds = false -- If set to false, it won't play the pain sounds
 ENT.GodMode = true -- Immune to everything
-//ENT.HasDeathRagdoll = false -- If set to false, it will not spawn the regular ragdoll of the SNPC
-ENT.DeathCorpseAlwaysCollide = true -- Should the corpse always collide?
+ENT.Bleeds = false -- Does the SNPC bleed? (Blood decal, particle, etc.)
 ENT.Immune_Dissolve = true -- Immune to Dissolving | Example: Combine Ball
 ENT.Immune_AcidPoisonRadiation = true -- Immune to Acid, Poison and Radiation
 ENT.Immune_Bullet = true -- Immune to Bullets
 ENT.Immune_Physics = true -- Immune to Physics
 ENT.ImmuneDamagesTable = {DMG_SLASH,DMG_GENERIC,DMG_CLUB,DMG_BULLET,DMG_BUCKSHOT,DMG_PHYSGUN} -- You can set Specific types of damages for the SNPC to be immune to
-ENT.DisableFindEnemy = true -- Disables FindEnemy code, friendly code still works though
 ENT.FindEnemy_UseSphere = true -- Should the SNPC be able to see all around him? (360) | Objects and walls can still block its sight!
-ENT.CallForHelp = false -- Does the SNPC call for help?
+ENT.DeathCorpseAlwaysCollide = true -- Should the corpse always collide?
+ENT.HasMeleeAttack = false -- Should the SNPC have a melee attack?
+ENT.DisableInitializeCapabilities = true -- If true, it will disable the initialize capabilities, this will allow you to add your own
+ENT.DisableSelectSchedule = true -- Disables Schedule code, Custom Schedule can still work
 ENT.DisableWandering = true -- Disables wandering when the SNPC is idle
+ENT.DisableFindEnemy = true -- Disables FindEnemy code, friendly code still works though
 ENT.BringFriendsOnDeath = false -- Should the SNPC's friends come to its position before it dies?
 ENT.CallForBackUpOnDamage = false -- Should the SNPC call for help when damaged? (Only happens if the SNPC hasn't seen a enemy)
 ENT.MoveOrHideOnDamageByEnemy = false -- Should the SNPC move or hide when being damaged by an enemy?
+ENT.CallForHelp = false -- Does the SNPC call for help?
+ENT.HasPainSounds = false -- If set to false, it won't play the pain sounds
 	-- ====== Sound File Paths ====== --
 -- Leave blank if you don't want any sounds to play
 ENT.SoundTbl_Death = {"ambient/explosions/explode_2.wav"}
-
--- Tank Base
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ Tank Base Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ENT.Tank_AngleDiffuseNumber = 180
 ENT.Tank_UseNegativeAngleDiffuseNumber = false
 ENT.Tank_AngleDiffuseGeneralNumber = 5
-ENT.Tank_UsesRightAngles = false
+ENT.Tank_UsesRightAngles = false -- For models that are rotated towards the right side
+
+	-- ====== SightVariables ====== --
 ENT.Tank_SeeClose = 350 -- If the enemy is closer than this number, than don't shoot!
 ENT.Tank_SeeFar = 5000 -- If the enemy is higher than this number, than don't shoot!
 ENT.Tank_SeeLimit = 6000 -- How far can it see?
 
--- Tank Shell Variables
+	-- ====== Tank Shell Variables ====== --
 ENT.Tank_Shell_TimeUntilFire = 2 -- How much time until it fires the shell?
 ENT.Tank_Shell_SpawnPos = Vector(-170,0,65)
 ENT.Tank_Shell_EntityToSpawn = "obj_vj_tank_shell" -- The entity that is spawned when the shell is fired
@@ -60,21 +58,30 @@ ENT.Tank_Shell_VelocitySpeed = 5000 -- How fast should the tank shell travel?
 ENT.Tank_Shell_DynamicLightPos = Vector(-200,0,0)
 ENT.Tank_Shell_MuzzleFlashPos = Vector(0,-235,18)
 ENT.Tank_Shell_ParticlePos = Vector(-205,00,72)
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ Customization Functions ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Use the functions below to customize certain parts of the base or to add new custom systems
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:CustomInitialize_CustomTank() end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:TankBase_CustomOnShellFire(Shell) end
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ ///// WARNING: Don't touch anything below this line! \\\\\ ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+util.AddNetworkString("vj_tankg_base_spawneffects")
+util.AddNetworkString("vj_tankg_base_shooteffects")
 
--- Independent Variables
 ENT.Tank_FacingTarget = false -- Is it facing the enemy?
 ENT.Tank_ShellReady = false -- Is the shell ready?
 ENT.Tank_ProperHeightShoot = false -- Is the enemy position proper height for it to shoot?
 ENT.Tank_ResetedEnemy = false
 ENT.Tank_GunnerIsTurning = false
 ENT.Tank_Status = 0
-
-util.AddNetworkString("vj_tankg_base_spawneffects")
-util.AddNetworkString("vj_tankg_base_shooteffects")
----------------------------------------------------------------------------------------------------------------------------------------------
-function ENT:CustomInitialize_CustomTank() end
----------------------------------------------------------------------------------------------------------------------------------------------
-function ENT:TankBase_CustomOnShellFire(Shell) end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialize()
 	self:CustomInitialize_CustomTank()
@@ -118,15 +125,16 @@ function ENT:CustomOnThink()
 	//if !self:GetParent() then self:Remove() end
 	if GetConVarNumber("vj_npc_noidleparticle") == 1 then return end
 	timer.Simple(0.1,function()
-	if self.Dead == false then
-	self:StartSpawnEffects() end end)
+		if IsValid(self) && self.Dead == false then
+			self:StartSpawnEffects()
+		end
+	end)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnThink_AIEnabled()
 	if self.Dead == true then return end
 	self:SetEnemy(self:GetParent():GetEnemy())
 
-	//print(self:GetEnemy())
 	//if self.Tank_ShellReady == true then print("Tank_ShellReady = true") else print("Tank_ShellReady = false") end
 	//if self.Tank_FacingTarget == true then print("Tank_FacingTarget = true") else print("Tank_FacingTarget = false") end
 	//if self.FiringShell == true then print("FiringShell = true") else print("FiringShell = false") end
@@ -141,40 +149,39 @@ function ENT:CustomOnThink_AIEnabled()
 	if self.Tank_ShellReady == false then self.FiringShell = false end
 	if self.Tank_Status == 0 then
 		if !IsValid(self:GetEnemy()) then
-		self.Tank_Status = 1
-		self.Tank_GunnerIsTurning = false
-	else
-	-- x = Forward | y = Right | z = Up
-	local Angle_Enemy = (self:GetEnemy():GetPos() - self:GetPos() /*+ Vector(80,0,80)*/):Angle()
-	local Angle_Current = self:GetAngles()
-	if self.Tank_UseNegativeAngleDiffuseNumber == true then
-	Angle_Diffuse = self:AngleDiffuse(Angle_Enemy.y,Angle_Current.y-self.Tank_AngleDiffuseNumber) else -- Cannon looking direction
-	Angle_Diffuse = self:AngleDiffuse(Angle_Enemy.y,Angle_Current.y+self.Tank_AngleDiffuseNumber) end -- Cannon looking direction
-	local Heigh_Ratio = (self:GetEnemy():GetPos().z - self:GetPos().z ) / self:GetPos():Distance(Vector(self:GetEnemy():GetPos().x,self:GetEnemy():GetPos().y,self:GetPos().z))
-	-- ^^^ How high it can shoot ^^^ --
-	if math.abs(Heigh_Ratio) < 0.15 then self.Tank_ProperHeightShoot = true else self.Tank_ProperHeightShoot = false end
-	self.Tank_GunnerIsTurning = false
-	if math.abs(Angle_Diffuse) < self.Tank_AngleDiffuseGeneralNumber && self.FiringShell == false && math.abs(Heigh_Ratio) < 0.15 && self:GetPos():Distance(self:GetEnemy():GetPos()) > self.Tank_SeeClose then
-	-- If the diffuse and the height and the enemy distance is higher than the self.Tank_SeeClose than shoot!
-		self.Tank_GunnerIsTurning = false
-		self.FiringShell = true
-		self.Tank_FacingTarget = true
-		if self:Visible(self:GetEnemy()) then
-		if GetConVarNumber("vj_npc_norange") == 0 then
-		self:RangeAttack_Base() end end
-	elseif Angle_Diffuse > self.Tank_AngleDiffuseGeneralNumber then
-		self:SetLocalAngles(self:GetLocalAngles() + Angle(0,2,0))
-		self.Tank_GunnerIsTurning = true
-		self.Tank_FacingTarget = false
-		self.FiringShell = false
-	elseif Angle_Diffuse < -self.Tank_AngleDiffuseGeneralNumber then
-		self:SetLocalAngles(self:GetLocalAngles() + Angle(0,-2,0))
-		self.Tank_GunnerIsTurning = true
-		self.Tank_FacingTarget = false
-		self.FiringShell = false
+			self.Tank_Status = 1
+			self.Tank_GunnerIsTurning = false
+		else
+			local Angle_Enemy = (self:GetEnemy():GetPos() - self:GetPos() /*+ Vector(80,0,80)*/):Angle()
+			local Angle_Current = self:GetAngles()
+			if self.Tank_UseNegativeAngleDiffuseNumber == true then
+			Angle_Diffuse = self:AngleDiffuse(Angle_Enemy.y,Angle_Current.y-self.Tank_AngleDiffuseNumber) else -- Cannon looking direction
+			Angle_Diffuse = self:AngleDiffuse(Angle_Enemy.y,Angle_Current.y+self.Tank_AngleDiffuseNumber) end -- Cannon looking direction
+			local Heigh_Ratio = (self:GetEnemy():GetPos().z - self:GetPos().z ) / self:GetPos():Distance(Vector(self:GetEnemy():GetPos().x,self:GetEnemy():GetPos().y,self:GetPos().z))
+			-- ^^^ How high it can shoot ^^^ --
+			if math.abs(Heigh_Ratio) < 0.15 then self.Tank_ProperHeightShoot = true else self.Tank_ProperHeightShoot = false end
+			self.Tank_GunnerIsTurning = false
+			if math.abs(Angle_Diffuse) < self.Tank_AngleDiffuseGeneralNumber && self.FiringShell == false && math.abs(Heigh_Ratio) < 0.15 && self:GetPos():Distance(self:GetEnemy():GetPos()) > self.Tank_SeeClose then
+			-- If the diffuse and the height and the enemy distance is higher than the self.Tank_SeeClose than shoot!
+				self.Tank_GunnerIsTurning = false
+				self.FiringShell = true
+				self.Tank_FacingTarget = true
+				if self:Visible(self:GetEnemy()) then
+				if GetConVarNumber("vj_npc_norange") == 0 then
+				self:RangeAttack_Base() end end
+			elseif Angle_Diffuse > self.Tank_AngleDiffuseGeneralNumber then
+				self:SetLocalAngles(self:GetLocalAngles() + Angle(0,5,0))
+				self.Tank_GunnerIsTurning = true
+				self.Tank_FacingTarget = false
+				self.FiringShell = false
+			elseif Angle_Diffuse < -self.Tank_AngleDiffuseGeneralNumber then
+				self:SetLocalAngles(self:GetLocalAngles() + Angle(0,-5,0))
+				self.Tank_GunnerIsTurning = true
+				self.Tank_FacingTarget = false
+				self.FiringShell = false
+			end
+		end
 	end
-  end
- end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnSchedule()
@@ -204,11 +211,11 @@ function ENT:CustomOnSchedule()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:TANK_MOVINGSOUND()
-	if self.HasSounds == true then if GetConVarNumber("vj_npc_sd_footstep") == 0 then
-	self.tank_movingsd = CreateSound(self, "vj_mili_tank/tank_gunnermove2_x.wav") self.tank_movingsd:SetSoundLevel(80)
+	if self.HasSounds == false or self.HasFootStepSound == false then return end
+	
+	self.tank_movingsd = CreateSound(self, "vj_mili_tank/tank_gunnermove2_x.wav")
+	self.tank_movingsd:SetSoundLevel(80)
 	self.tank_movingsd:PlayEx(1,100)
-	end
- end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:RangeAttack_Base()
