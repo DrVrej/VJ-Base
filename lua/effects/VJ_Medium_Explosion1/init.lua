e461f4a5faa0c976b8939f1cb60c49c4e602476f
DrVrej <DrVrej@users.noreply.github.com>
2023-01-16 21:45:29 -0500

    CR Date Update

--- lua/effects/VJ_Medium_Explosion1/init.lua
+++ lua/effects/VJ_Medium_Explosion1/init.lua
@@ -1,6 +1,6 @@
 if (!file.Exists("autorun/vj_base_autorun.lua","LUA")) then return end
 /*--------------------------------------------------
-	*** Copyright (c) 2012-2022 by DrVrej, All rights reserved. ***
+	*** Copyright (c) 2012-2023 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 --------------------------------------------------*/
