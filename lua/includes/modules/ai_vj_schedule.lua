6ed4b4d5f60b642f9f34ae5d6c27d7f56fe6f6f8
DrVrej <DrVrej@users.noreply.github.com>
2023-03-24 03:36:52 -0400

    Update AI modules

--- lua/includes/modules/ai_vj_schedule.lua
+++ lua/includes/modules/ai_vj_schedule.lua
@@ -1,51 +1,50 @@
 /*--------------------------------------------------
-	=============== VJ AI Schedule Module ===============
 	*** Copyright (c) 2012-2023 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
-INFO: This is mostly Garry's code, except I improved and organized.
-NOTES: This is server side only!
 --------------------------------------------------*/
 if CLIENT then return end
 
-print("VJ Base A.I. module initialized!")
-
-local setmetatable 	= setmetatable
-local tostring 		= tostring
-local table			= table
+print("VJ Base module: AI Schedules initialized!")
 
 require("ai_vj_task")
+local setmetatable = setmetatable
+local tostring = tostring
+local table = table
 local ai_vj_task 		= ai_vj_task
-module("ai_vj_schedule")
 
-local Schedule		= {}
-Schedule.__index 	= Schedule
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------ Begin Metatable ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+module("ai_vj_schedule")
+local Schedule = {}
+Schedule.__index = Schedule
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function Schedule:Init(_debugname_)
-	self.DebugName 	= tostring(_debugname_)
-	self.Tasks 		= {}
-	self.TaskCount 	= 0
+function Schedule:Init(debugName)
+	self.debugName = tostring(debugName)
+	self.Tasks = {}
+	self.TaskCount = 0
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function Schedule:EngTask(_taskname_,_taskdata_)
+function Schedule:EngTask(taskName, taskData)
 	local NewTask = ai_vj_task.New()
-	NewTask:InitEngine(_taskname_,_taskdata_)
-	table.insert(self.Tasks,NewTask)
+	NewTask:InitEngine(taskName, taskData)
+	table.insert(self.Tasks, NewTask)
 	self.TaskCount = self.TaskCount + 1
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function Schedule:AddTask(_functionname_,_data_)
+function Schedule:AddTask(funcName, data)
 	local NewTask = ai_vj_task.New()
-	NewTask:InitFunctionName("TaskStart_".._functionname_,"Task_".._functionname_,_data_)
-	table.insert(self.Tasks,NewTask)
+	NewTask:InitFunctionName("TaskStart_"..funcName, "Task_"..funcName, data)
+	table.insert(self.Tasks, NewTask)
 	//print(self.TaskCount.." AddTask has ran!")
 	self.TaskCount = self.TaskCount + 1  -- Or else it will make an error saying NumTasks is a nil value
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function Schedule:AddTaskEx(_start,_run,_data_)
+function Schedule:AddTaskEx(startFunc, endFunc, data)
 	local NewTask = ai_vj_task.New()
-	NewTask:InitFunctionName(_start, _run, _data_)
-	table.insert(self.Tasks,NewTask)
+	NewTask:InitFunctionName(startFunc, endFunc, data)
+	table.insert(self.Tasks, NewTask)
 	self.TaskCount = self.TaskCount + 2  -- Or else it will make an error saying NumTasks is a nil value
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -57,9 +56,9 @@ function Schedule:GetTask(num)
 	return self.Tasks[num]
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function New(debugname)
-	local pNewSchedule = {}
-	setmetatable(pNewSchedule,Schedule)
-	pNewSchedule:Init(debugname)
-	return pNewSchedule
+function New(debugName)
+	local newSchedule = {}
+	setmetatable(newSchedule, Schedule)
+	newSchedule:Init(debugName)
+	return newSchedule
 end
