5fc828484a7452d932905c9571026edd48fab737
DrVrej <DrVrej@users.noreply.github.com>
2023-04-02 00:32:45 -0400

    Small optimizations

--- lua/entities/npc_vj_human_base/init.lua
+++ lua/entities/npc_vj_human_base/init.lua
@@ -4032,75 +4032,75 @@ function ENT:CreateDeathCorpse(dmginfo, hitgroup)
 		else
 			corpseType = self.DeathCorpseEntityClass
 		end
-		//if self.VJCorpseDeleted == true then
-		self.Corpse = ents.Create(corpseType) //end
-		self.Corpse:SetModel(corpseMdl)
-		self.Corpse:SetPos(self:GetPos())
-		self.Corpse:SetAngles(self:GetAngles())
-		self.Corpse:Spawn()
-		self.Corpse:Activate()
-		self.Corpse:SetColor(self:GetColor())
-		self.Corpse:SetMaterial(self:GetMaterial())
+		self.Corpse = ents.Create(corpseType)
+		local corpse = self.Corpse
+		corpse:SetModel(corpseMdl)
+		corpse:SetPos(self:GetPos())
+		corpse:SetAngles(self:GetAngles())
+		corpse:Spawn()
+		corpse:Activate()
+		corpse:SetColor(self:GetColor())
+		corpse:SetMaterial(self:GetMaterial())
 		if corpseMdlCustom == false && self.DeathCorpseSubMaterials != nil then -- Take care of sub materials
 			for _, x in ipairs(self.DeathCorpseSubMaterials) do
 				if self:GetSubMaterial(x) != "" then
-					self.Corpse:SetSubMaterial(x, self:GetSubMaterial(x))
+					corpse:SetSubMaterial(x, self:GetSubMaterial(x))
 				end
 			end
 			 -- This causes lag, not a very good way to do it.
 			/*for x = 0, #self:GetMaterials() do
 				if self:GetSubMaterial(x) != "" then
-					self.Corpse:SetSubMaterial(x, self:GetSubMaterial(x))
+					corpse:SetSubMaterial(x, self:GetSubMaterial(x))
 				end
 			end*/
 		end
-		//self.Corpse:SetName("self.Corpse" .. self:EntIndex())
-		//self.Corpse:SetModelScale(self:GetModelScale())
-		self.Corpse.FadeCorpseType = (self.Corpse:GetClass() == "prop_ragdoll" and "FadeAndRemove") or "kill"
-		self.Corpse.IsVJBaseCorpse = true
-		self.Corpse.DamageInfo = dmginfo
-		self.Corpse.ExtraCorpsesToRemove = self.ExtraCorpsesToRemove_Transition
-		self.Corpse.BloodData = {Color = self.BloodColor, Particle = self.CustomBlood_Particle, Decal = self.CustomBlood_Decal}
+		//corpse:SetName("corpse" .. self:EntIndex())
+		//corpse:SetModelScale(self:GetModelScale())
+		corpse.FadeCorpseType = (corpse:GetClass() == "prop_ragdoll" and "FadeAndRemove") or "kill"
+		corpse.IsVJBaseCorpse = true
+		corpse.DamageInfo = dmginfo
+		corpse.ExtraCorpsesToRemove = self.ExtraCorpsesToRemove_Transition
+		corpse.BloodData = {Color = self.BloodColor, Particle = self.CustomBlood_Particle, Decal = self.CustomBlood_Decal}
 
 		if self.Bleeds == true && self.HasBloodPool == true && GetConVar("vj_npc_nobloodpool"):GetInt() == 0 then
 			self:SpawnBloodPool(dmginfo, hitgroup)
 		end
 		
 		-- Collision --
-		self.Corpse:SetCollisionGroup(self.DeathCorpseCollisionType)
+		corpse:SetCollisionGroup(self.DeathCorpseCollisionType)
 		if GetConVar("ai_serverragdolls"):GetInt() == 1 then
-			undo.ReplaceEntity(self, self.Corpse)
+			undo.ReplaceEntity(self, corpse)
 		else -- Keep corpses is not enabled...
-			VJ_AddCorpse(self.Corpse)
-			//hook.Call("VJ_CreateSNPCCorpse", nil, self.Corpse, self)
-			if GetConVar("vj_npc_undocorpse"):GetInt() == 1 then undo.ReplaceEntity(self, self.Corpse) end -- Undoable
+			VJ_AddCorpse(corpse)
+			//hook.Call("VJ_CreateSNPCCorpse", nil, corpse, self)
+			if GetConVar("vj_npc_undocorpse"):GetInt() == 1 then undo.ReplaceEntity(self, corpse) end -- Undoable
 		end
-		cleanup.ReplaceEntity(self, self.Corpse) -- Delete on cleanup
+		cleanup.ReplaceEntity(self, corpse) -- Delete on cleanup
 		
 		-- Miscellaneous --
-		self.Corpse:SetSkin((self.DeathCorpseSkin == -1 and self:GetSkin()) or self.DeathCorpseSkin)
+		corpse:SetSkin((self.DeathCorpseSkin == -1 and self:GetSkin()) or self.DeathCorpseSkin)
 		
 		if self.DeathCorpseSetBodyGroup == true then -- Yete asega true-e, ooremen gerna bodygroup tenel
 			for i = 0,18 do -- 18 = Bodygroup limit
-				self.Corpse:SetBodygroup(i,self:GetBodygroup(i))
+				corpse:SetBodygroup(i,self:GetBodygroup(i))
 			end
 			if self.DeathCorpseBodyGroup.a != -1 then -- Yete asiga nevaz meg chene, user-in teradz tevere kordzadze
-				self.Corpse:SetBodygroup(self.DeathCorpseBodyGroup.a, self.DeathCorpseBodyGroup.b)
+				corpse:SetBodygroup(self.DeathCorpseBodyGroup.a, self.DeathCorpseBodyGroup.b)
 			end
 		end
 		
 		if self:IsOnFire() then -- If was on fire then...
-			self.Corpse:Ignite(math.Rand(8, 10), 0)
-			self.Corpse:SetColor(colorGrey)
-			//self.Corpse:SetMaterial("models/props_foliage/tree_deciduous_01a_trunk")
+			corpse:Ignite(math.Rand(8, 10), 0)
+			corpse:SetColor(colorGrey)
+			//corpse:SetMaterial("models/props_foliage/tree_deciduous_01a_trunk")
 		end
-		//gamemode.Call("CreateEntityRagdoll",self,self.Corpse)
+		//gamemode.Call("CreateEntityRagdoll",self,corpse)
 
 		-- Dissolve --
 		if (bit.band(self.SavedDmgInfo.type, DMG_DISSOLVE) != 0) or (IsValid(self.SavedDmgInfo.inflictor) && self.SavedDmgInfo.inflictor:GetClass() == "prop_combine_ball") then
-			self.Corpse:SetName("vj_dissolve_corpse")
+			corpse:SetName("vj_dissolve_corpse")
 			local dissolver = ents.Create("env_entity_dissolver")
-			dissolver:SetPos(self.Corpse:GetPos())
+			dissolver:SetPos(corpse:GetPos())
 			dissolver:Spawn()
 			dissolver:Activate()
 			//dissolver:SetKeyValue("target","vj_dissolve_corpse")
@@ -4124,18 +4124,18 @@ function ENT:CreateDeathCorpse(dmginfo, hitgroup)
 			dmgForce = self:GetMoveVelocity() == defPos and self:GetGroundSpeedVelocity() or self:GetMoveVelocity()
 		end
 		local totalSurface = 0
-		local physCount = self.Corpse:GetPhysicsObjectCount()
+		local physCount = corpse:GetPhysicsObjectCount()
 		for boneLimit = 0, physCount - 1 do -- 128 = Bone Limit
-			local childphys = self.Corpse:GetPhysicsObjectNum(boneLimit)
+			local childphys = corpse:GetPhysicsObjectNum(boneLimit)
 			if IsValid(childphys) then
 				totalSurface = totalSurface + childphys:GetSurfaceArea()
-				local childphys_bonepos, childphys_boneang = self:GetBonePosition(self.Corpse:TranslatePhysBoneToBone(boneLimit))
+				local childphys_bonepos, childphys_boneang = self:GetBonePosition(corpse:TranslatePhysBoneToBone(boneLimit))
 				if (childphys_bonepos) then
 					//if math.Round(math.abs(childphys_boneang.r)) != 90 then -- Fixes ragdolls rotating, no longer needed!    --->    sv_pvsskipanimation 0
 						if self.DeathCorpseSetBoneAngles == true then childphys:SetAngles(childphys_boneang) end
 						childphys:SetPos(childphys_bonepos)
 					//end
-					if self.Corpse:GetName() == "vj_dissolve_corpse" then
+					if corpse:GetName() == "vj_dissolve_corpse" then
 						childphys:EnableGravity(false)
 						childphys:SetVelocity(self:GetForward()*-150 + self:GetRight()*math.Rand(100,-100) + self:GetUp()*50)
 					else
@@ -4144,38 +4144,38 @@ function ENT:CreateDeathCorpse(dmginfo, hitgroup)
 						end
 					end
 				elseif physCount == 1 then -- If it's only 1, then it's likely a regular physics model with no bones
-					if self.Corpse:GetName() == "vj_dissolve_corpse" then
+					if corpse:GetName() == "vj_dissolve_corpse" then
 						childphys:EnableGravity(false)
 						childphys:SetVelocity(self:GetForward()*-150 + self:GetRight()*math.Rand(100,-100) + self:GetUp()*50)
 					else
 						if self.DeathCorpseApplyForce == true /*&& self.DeathAnimationCodeRan == false*/ then
-							childphys:SetVelocity(dmgForce / math.max(1, (useLocalVel and self.Corpse:GetPos():Distance(self.SavedDmgInfo.pos)/12) or 1))
+							childphys:SetVelocity(dmgForce / math.max(1, (useLocalVel and corpse:GetPos():Distance(self.SavedDmgInfo.pos)/12) or 1))
 						end
 					end
 				end
 			end
 		end
 		
-		if self.Corpse:Health() <= 0 then
-			local hpCalc = totalSurface / 60 // self.Corpse:OBBMaxs():Distance(self.Corpse:OBBMins())
-			self.Corpse:SetMaxHealth(hpCalc)
-			self.Corpse:SetHealth(hpCalc)
+		if corpse:Health() <= 0 then
+			local hpCalc = totalSurface / 60 // corpse:OBBMaxs():Distance(corpse:OBBMins())
+			corpse:SetMaxHealth(hpCalc)
+			corpse:SetHealth(hpCalc)
 		end
-		VJ_AddStinkyEnt(self.Corpse, true)
+		VJ_AddStinkyEnt(corpse, true)
 	
-		if IsValid(self.TheDroppedWeapon) then self.Corpse.ExtraCorpsesToRemove[#self.Corpse.ExtraCorpsesToRemove+1] = self.TheDroppedWeapon end
-		if self.DeathCorpseFade == true then self.Corpse:Fire(self.Corpse.FadeCorpseType,"",self.DeathCorpseFadeTime) end
-		if GetConVar("vj_npc_corpsefade"):GetInt() == 1 then self.Corpse:Fire(self.Corpse.FadeCorpseType,"",GetConVar("vj_npc_corpsefadetime"):GetInt()) end
-		self:CustomOnDeath_AfterCorpseSpawned(dmginfo, hitgroup, self.Corpse)
-		self.Corpse:CallOnRemove("vj_"..self.Corpse:EntIndex(),function(ent,exttbl)
+		if IsValid(self.TheDroppedWeapon) then corpse.ExtraCorpsesToRemove[#corpse.ExtraCorpsesToRemove+1] = self.TheDroppedWeapon end
+		if self.DeathCorpseFade == true then corpse:Fire(corpse.FadeCorpseType,"",self.DeathCorpseFadeTime) end
+		if GetConVar("vj_npc_corpsefade"):GetInt() == 1 then corpse:Fire(corpse.FadeCorpseType,"",GetConVar("vj_npc_corpsefadetime"):GetInt()) end
+		self:CustomOnDeath_AfterCorpseSpawned(dmginfo, hitgroup, corpse)
+		corpse:CallOnRemove("vj_"..corpse:EntIndex(),function(ent,exttbl)
 			for _,v in ipairs(exttbl) do
 				if IsValid(v) then
 					if v:GetClass() == "prop_ragdoll" then v:Fire("FadeAndRemove","",0) else v:Fire("kill","",0) end
 				end
 			end
-		end,self.Corpse.ExtraCorpsesToRemove)
-		hook.Call("CreateEntityRagdoll", nil, self, self.Corpse)
-		return self.Corpse
+		end,corpse.ExtraCorpsesToRemove)
+		hook.Call("CreateEntityRagdoll", nil, self, corpse)
+		return corpse
 	else
 		if IsValid(self.TheDroppedWeapon) then self.TheDroppedWeapon:Remove() end
 		for _,v in ipairs(self.ExtraCorpsesToRemove_Transition) do
