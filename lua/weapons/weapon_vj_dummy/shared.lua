b0fb3e6ffcc68fb4efb9a57e50be02a42a62c914
Vrej <DrVrej@users.noreply.github.com>
2023-05-08 17:11:23 -0400

    Removed file checks to speed up loading

--- lua/weapons/weapon_vj_dummy/shared.lua
+++ lua/weapons/weapon_vj_dummy/shared.lua
@@ -1,5 +1,3 @@
-if (!file.Exists("autorun/vj_base_autorun.lua","LUA")) then return end
----------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.Base = "weapon_vj_base"
 SWEP.PrintName = "Dummy Weapon"
 SWEP.Author = "DrVrej"
