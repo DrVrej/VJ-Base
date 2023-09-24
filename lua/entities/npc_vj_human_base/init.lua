2e29d616ca4dc7cae9c75e2901233b1ce09f9f00
Cpt-Hazama <Caleb123459@gmail.com>
2023-09-24 00:46:29 -0400

    Goofy aa removal

--- lua/entities/npc_vj_human_base/init.lua
+++ lua/entities/npc_vj_human_base/init.lua
@@ -3071,14 +3071,8 @@ function ENT:ThrowGrenadeCode(customEnt, noOwner)
 				getSpawnPos = self:CustomOnGrenadeAttack_SpawnPosition()
 				getSpawnAngle = getSpawnPos:Angle()
 			else
-				local att = self:LookupAttachment(self.GrenadeAttackAttachment)
-				if att > 0 then
-					getSpawnPos = self:GetAttachment(att).Pos
-					getSpawnAngle = self:GetAttachment(att).Ang
-				else
-					getSpawnPos = self:GetShootPos()
-					getSpawnAngle = self:GetAngles()
-				end
+				getSpawnPos = self:GetAttachment(self:LookupAttachment(self.GrenadeAttackAttachment)).Pos
+				getSpawnAngle = self:GetAttachment(self:LookupAttachment(self.GrenadeAttackAttachment)).Ang
 			end
 	
 			local greTargetPos = self:GetPos() + self:GetForward()*200
