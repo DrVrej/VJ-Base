c51476432d503c987a1038fdcaa85f5460b10739
Vrej <DrVrej@users.noreply.github.com>
2024-07-01 01:14:06 -0400

    Update init.lua

--- lua/entities/npc_vj_dum_dummy/init.lua
+++ lua/entities/npc_vj_dum_dummy/init.lua
@@ -1,5 +1,5 @@
 AddCSLuaFile("shared.lua")
-include('shared.lua')
+include("shared.lua")
 
 ENT.Model = {} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 20 -- or you can use a convar: GetConVarNumber("vj_dum_dummy_h")
