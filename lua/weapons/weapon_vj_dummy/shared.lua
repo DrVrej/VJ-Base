bd95bd9eed1e08583b684b539e59fd558f8286f5
Vrej <DrVrej@users.noreply.github.com>
2025-02-18 01:45:13 -0500

    Update shared.lua

--- lua/weapons/weapon_vj_dummy/shared.lua
+++ lua/weapons/weapon_vj_dummy/shared.lua
@@ -2,8 +2,6 @@ SWEP.Base = "weapon_vj_base"
 SWEP.PrintName = "Dummy Weapon"
 SWEP.Author = "DrVrej"
 SWEP.Contact = "http://steamcommunity.com/groups/vrejgaming"
-SWEP.Purpose = "This weapon is made for Players and NPCs"
-SWEP.Instructions = "Controls are like a regular weapon."
 SWEP.Category = "VJ Base Dummy"
 	-- Main Settings ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.ViewModel = "" -- The view model (First person)
