3e2769f4aa3ef3159911a9b53b57e048b95c3ef1
Vrej <DrVrej@users.noreply.github.com>
2025-02-18 00:53:13 -0500

    Update shared.lua

--- lua/entities/npc_vj_dum_dummy/shared.lua
+++ lua/entities/npc_vj_dum_dummy/shared.lua
@@ -1,8 +1,6 @@
-ENT.Base 			= "npc_vj_creature_base" -- List of all base types: https://github.com/DrVrej/VJ-Base/wiki/Base-Types
+ENT.Base 			= "npc_vj_creature_base"
 ENT.Type 			= "ai"
 ENT.PrintName 		= "Dummy NPC"
 ENT.Author 			= "DrVrej"
 ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
-ENT.Purpose 		= "Spawn it and fight with it!"
-ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "VJ Base Dummy"
