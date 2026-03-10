8bf51443b63851373ecfeac7921d5f048260ae8d
SuNdee2004 <136211546+SuNdee2004@users.noreply.github.com>
2026-03-10 20:51:45 +0800

    Controlling NPC HUD Display
    It can automatically adjust its panel's width size depending on npc's name

--- lua/entities/obj_vj_controller/shared.lua
+++ lua/entities/obj_vj_controller/shared.lua
@@ -213,6 +213,9 @@ local draw_SimpleText = draw.SimpleText
 local surface_SetMaterial = surface.SetMaterial
 local surface_SetDrawColor = surface.SetDrawColor
 local surface_DrawTexturedRect = surface.DrawTexturedRect
+local surface_SetFont = surface.SetFont
+local surface_GetTextSize = surface.GetTextSize
+local math_max = math.max
 local ScrW, ScrH = ScrW, ScrH
 --
 function ENT:HUD()
@@ -229,49 +232,85 @@ function ENT:HUD()
 	local atkWeapon = IsValid(self:GetNPCWeapon())
 	local atkWeaponAmmo = self:GetNPCWeaponAmmo()
 	
-	draw_RoundedBox(box_roundness, srcW / 2.24, srcH - 130, 215, 100, color_box)
-	draw_SimpleText(self:GetNPCName(), "VJBaseSmallMedium", srcW / 2.21, srcH - 125, color_white, 0, 0)
+	-- Calculate NPC Name Width
+	local npcName = self:GetNPCName()
+	surface_SetFont("VJBaseSmallMedium")
+	local nameWidth, _ = surface_GetTextSize(npcName)
+	local boxWidth = math_max(215, nameWidth + 35) -- Automatically adapt if text is longer than 215 width
+	
+	local boxX = srcW / 2.24
+	local boxCenterX = boxX + (boxWidth / 2)
+	
+	-- Background Panel
+	draw_RoundedBox(box_roundness, boxX, srcH - 130, boxWidth, 100, color_box)
+	draw_SimpleText(npcName, "VJBaseSmallMedium", boxCenterX, srcH - 125, color_white, 1, 0)
 	
 	-- Health
 	lerp_hp = Lerp(8 * FrameTime(), lerp_hp, health)
-	draw_RoundedBox(box_roundness, srcW / 2.21, srcH - 105, 190, 20, color_cyan_muted)
-	draw_RoundedBox(box_roundness, srcW / 2.21 + box_border_thickness, srcH - 105 + box_border_thickness, 190 - box_border_thickness * 2, 20 - box_border_thickness * 2, color_box_under)
-	draw_RoundedBox(box_roundness, srcW / 2.21 + box_border_thickness, srcH - 105 + box_border_thickness, ((190 * math_clamp(lerp_hp, 0, healthMax)) / healthMax) - box_border_thickness * 2, 20 - box_border_thickness * 2, color_cyan_muted)
-	draw_SimpleText(string.format("%.0f",  lerp_hp) .. "/" .. healthMax,  "VJBaseSmallMedium", (srcW / 1.99) - ((surface.GetTextSize(health .. "/" .. healthMax)) / 2), srcH - 103, color_white)
+	local hpBarWidth = 190
+	local hpBarX = boxCenterX - (hpBarWidth / 2)
+	draw_RoundedBox(box_roundness, hpBarX, srcH - 105, hpBarWidth, 20, color_cyan_muted)
+	draw_RoundedBox(box_roundness, hpBarX + box_border_thickness, srcH - 105 + box_border_thickness, hpBarWidth - box_border_thickness * 2, 20 - box_border_thickness * 2, color_box_under)
+	draw_RoundedBox(box_roundness, hpBarX + box_border_thickness, srcH - 105 + box_border_thickness, ((hpBarWidth * math_clamp(lerp_hp, 0, healthMax)) / healthMax) - box_border_thickness * 2, 20 - box_border_thickness * 2, color_cyan_muted)
+	draw_SimpleText(string.format("%.0f",  lerp_hp) .. "/" .. healthMax,  "VJBaseSmallMedium", boxCenterX, srcH - 103, color_white, 1, 0)
 	
 	-- Attack Icons
+	surface_SetFont("VJBaseMedium")
+	local ammoText = tostring(atkWeaponAmmo)
+	local ammoWidth = 0
+	if atkWeapon then
+		ammoWidth, _ = surface_GetTextSize(ammoText)
+		ammoWidth = ammoWidth + 5 -- Add padding
+	end
+	
+	local iconSize = 28
+	local iconGap = 6
+	local totalIconWidth = (iconSize * 5) + (iconGap * 4) + ammoWidth
+	local iconStartX = boxCenterX - (totalIconWidth / 2)
+	
 	surface_SetMaterial(mat_icon_melee)
 	surface_SetDrawColor(attack_icon_color[atkMelee] or color_green)
-	surface_DrawTexturedRect(srcW / 2.21, srcH - 83, 28, 28)
+	surface_DrawTexturedRect(iconStartX, srcH - 83, iconSize, iconSize)
 	
 	surface_SetMaterial(mat_icon_range)
 	surface_SetDrawColor(attack_icon_color[atkRange] or color_green)
-	surface_DrawTexturedRect(srcW / 2.14, srcH - 83, 28, 28)
+	surface_DrawTexturedRect(iconStartX + (iconSize + iconGap), srcH - 83, iconSize, iconSize)
 	
 	surface_SetMaterial(mat_icon_leap)
 	surface_SetDrawColor(attack_icon_color[atkLeap] or color_green)
-	surface_DrawTexturedRect(srcW / 2.065, srcH - 83, 28, 28)
+	surface_DrawTexturedRect(iconStartX + (iconSize + iconGap) * 2, srcH - 83, iconSize, iconSize)
 	
 	surface_SetMaterial(mat_icon_grenade)
 	surface_SetDrawColor(attack_icon_color[atkGrenade] or color_green)
-	surface_DrawTexturedRect(srcW / 2.005, srcH - 83, 28, 28)
+	surface_DrawTexturedRect(iconStartX + (iconSize + iconGap) * 3, srcH - 83, iconSize, iconSize)
 	
 	surface_SetMaterial(mat_icon_gun)
 	surface_SetDrawColor((!atkWeapon and color_red) or ((atkWeaponAmmo <= 0 and color_orange) or color_green))
-	surface_DrawTexturedRect(srcW / 1.94, srcH - 83, 28, 28)
+	surface_DrawTexturedRect(iconStartX + (iconSize + iconGap) * 4, srcH - 83, iconSize, iconSize)
 	if atkWeapon then
-		draw_SimpleText(atkWeaponAmmo, "VJBaseMedium", srcW / 1.885, srcH - 80, (atkWeaponAmmo <= 0 and color_orange) or color_green, 0, 0)
+		draw_SimpleText(ammoText, "VJBaseMedium", iconStartX + (iconSize + iconGap) * 4 + iconSize + 5, srcH - 80, (atkWeaponAmmo <= 0 and color_orange) or color_green, 0, 0)
 	end
 	
-	-- Camera Mode
+	-- Camera Mode & Zoom
+	local modeText = (self:GetCameraMode() == 1 and "Third") or "First"
+	local zoomText = tostring(self.VJC_Camera_Zoom)
+	local modeWidth, _ = surface_GetTextSize(modeText)
+	local zoomWidth, _ = surface_GetTextSize(zoomText)
+	local camIconSize = 22
+	local camTextGap = 5
+	local camSectionGap = 20
+	
+	local totalCamWidth = camIconSize + camTextGap + modeWidth + camSectionGap + camIconSize + camTextGap + zoomWidth
+	local camStartX = boxCenterX - (totalCamWidth / 2)
+	
 	surface_SetMaterial(mat_icon_camera)
 	surface_SetDrawColor(color_white)
-	surface_DrawTexturedRect(srcW / 2.21, srcH - 55, 22, 22)
-	draw_SimpleText((self:GetCameraMode() == 1 and "Third") or "First", "VJBaseMedium", srcW / 2.14, srcH - 55, color_white, 0, 0)
+	surface_DrawTexturedRect(camStartX, srcH - 55, camIconSize, camIconSize)
+	draw_SimpleText(modeText, "VJBaseMedium", camStartX + camIconSize + camTextGap, srcH - 55, color_white, 0, 0)
 	
-	-- Camera Zoom
+	local zoomStartX = camStartX + camIconSize + camTextGap + modeWidth + camSectionGap
 	surface_SetMaterial(mat_icon_zoom)
 	surface_SetDrawColor(color_white)
-	surface_DrawTexturedRect(srcW / 1.94, srcH - 55, 22, 22)
-	draw_SimpleText(self.VJC_Camera_Zoom, "VJBaseMedium", srcW / 1.885, srcH - 55, color_white, 0, 0)
+	surface_DrawTexturedRect(zoomStartX, srcH - 55, camIconSize, camIconSize)
+	draw_SimpleText(zoomText, "VJBaseMedium", zoomStartX + camIconSize + camTextGap, srcH - 55, color_white, 0, 0)
 end
