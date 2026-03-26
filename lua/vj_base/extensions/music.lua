4a9effc0b9e202e0a0a894bcb83427e9cb78bb59
DrVrej <DrVrej@users.noreply.github.com>
2026-03-27 03:16:06 +0500

    Fixed soundtracks playing at full volume for 1 second when the NPC is created

--- lua/vj_base/extensions/music.lua
+++ lua/vj_base/extensions/music.lua
@@ -5,13 +5,39 @@
 -----------------------------------------------*/
 if !CLIENT then return end
 
+VJ.Music_Queue = {}
+--
 local IsValid = IsValid
 local table_remove = table.remove
-
 local vj_npc_snd_track_volume = GetConVar("vj_npc_snd_track_volume")
-
-VJ.Music_Queue = {}
-
+--
+local function VJ_Music_Tick()
+	//PrintTable(VJ.Music_Queue)
+	-- Clean up the music queue by removing any entries that have an invalid NPC
+	for k, v in pairs(VJ.Music_Queue) do
+		if !IsValid(v.npc) then
+			v.channel:Stop()
+			v.channel = nil
+			table_remove(VJ.Music_Queue, k)
+		end
+	end
+	-- No music exists, so stop the thinking
+	if #VJ.Music_Queue <= 0 then
+		timer.Remove("vj_music_think")
+		VJ.Music_Queue = {}
+	else
+		for _, v in pairs(VJ.Music_Queue) do
+			local chan = v.channel
+			if IsValid(v.npc) && IsValid(chan) then
+				local volOverride = vj_npc_snd_track_volume:GetFloat()
+				chan:SetVolume(volOverride == 1 and v.npcVolume or volOverride)
+				chan:Play()
+				break
+			end
+		end
+	end
+end
+--
 net.Receive("vj_music_cl", function(len)
 	local ent = net.ReadEntity()
 	local sdFile = net.ReadString()
@@ -20,38 +46,14 @@ net.Receive("vj_music_cl", function(len)
 	-- Flags: "noplay" = Forces the sound not to play as soon as this function is called
 	sound.PlayFile("sound/" .. sdFile, "noplay", function(sdChan, errorID, errorName)
 		if IsValid(sdChan) then
-			if #VJ.Music_Queue <= 0 then sdChan:Play() end
+			//if #VJ.Music_Queue <= 0 then sdChan:Play() end -- Plays too soon for menu volume setting to be applied
 			sdChan:EnableLooping(true)
 			sdChan:SetPlaybackRate(sdPlayback)
 			table.insert(VJ.Music_Queue, {npc = ent, channel = sdChan, npcVolume = sdVol})
+			VJ_Music_Tick()
 		else
 			print("[VJ Base Music] Error adding soundtrack!", errorID, errorName)
 		end
 	end)
-	timer.Create("vj_music_think", 1, 0, function()
-		//PrintTable(VJ.Music_Queue)
-		for k, v in pairs(VJ.Music_Queue) do
-			//PrintTable(v)
-			if !IsValid(v.npc) then
-				v.channel:Stop()
-				v.channel = nil
-				table_remove(VJ.Music_Queue, k)
-			end
-		end
-		-- No music exists, so stop the thinking
-		if #VJ.Music_Queue <= 0 then
-			timer.Remove("vj_music_think")
-			VJ.Music_Queue = {}
-		else
-			for _, v in pairs(VJ.Music_Queue) do
-				local chan = v.channel
-				if IsValid(v.npc) && IsValid(chan) then
-					local volOverride = vj_npc_snd_track_volume:GetFloat()
-					chan:SetVolume(volOverride == 1 and v.npcVolume or volOverride)
-					chan:Play()
-					break
-				end
-			end
-		end
-	end)
+	timer.Create("vj_music_think", 1, 0, VJ_Music_Tick)
 end)
